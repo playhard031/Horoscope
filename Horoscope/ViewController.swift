8379d84e3af5cee5ca61a348527d6620d343e567
agrmaster <63108745+agrmaster@users.noreply.github.com>
2021-08-03 19:23:27 +0500

    Initial Commit

---
+++ Horoscope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Horoscope
+//
+//  Created by Андрей Сергеевич on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
